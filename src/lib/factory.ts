96a03a136f98750f08fc4c98af3f7846dd642f83
MrBoxs <rizkywanda200155@gmail.com>
2025-11-29 19:16:41 +0700

    feat: Integrate Prisma ORM, adding database configuration, schema definition, and client setup.

--- src/lib/factory.ts
+++ src/lib/factory.ts
@@ -1,8 +1,19 @@
 import { createFactory } from 'hono/factory';
 
-const factory = createFactory({
+import { prisma } from '@/lib/prisma';
+
+import type { AppBindings } from '@/types/app-bindings.type';
+
+const factory = createFactory<AppBindings>({
   defaultAppOptions: {
     strict: false,
+  },
+
+  initApp: (app) => {
+    app.use(async (c, next) => {
+      c.set('prisma', prisma);
+      await next();
+    });
   },
 });
 
---
+++ src/lib/prisma.ts
@@ -0,0 +1,14 @@
+import { env } from '@/config/env';
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { PrismaClient } from '@/generated/prisma/client';
+
+const adapter = new PrismaNeon({ connectionString: env.DATABASE_URL });
+const createPrismaClient = () => new PrismaClient({ adapter });
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: ReturnType<typeof createPrismaClient> | undefined;
+};
+
+export const prisma = globalForPrisma.prisma ?? createPrismaClient();
+
+if (env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
